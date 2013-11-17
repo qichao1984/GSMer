a7f6bc26ac1942dfb8ec768022229ad20fff9ddd
Qichao Tu <philloid@gmail.com>
2013-11-17 17:22:33 -0600

    Create GSMer.pl

---
+++ GSMer.pl
@@ -0,0 +1,18 @@
+#
+#===============================================================================
+#
+#         FILE: GSMer.pl
+#
+#  DESCRIPTION: This program identifies genome-specific markers from currently
+#  sequenced micrbial genomes using k-mer based approaches.
+#
+#        FILES: GSMer.pl
+#         BUGS: ---
+#        NOTES: Exploring genome-specific markers for strain/species identification
+#        in metagenomes
+#       AUTHOR: Qichao Tu (qichao@ou.edu, philloid@gmail.com)
+#  INSTITUTION: Institute for Environmental Genomics, University of Oklahoma
+#      VERSION: 1.0
+#      CREATED: 11/17/2013 10:28:38 AM
+#     REVISION: ---
+#===============================================================================
